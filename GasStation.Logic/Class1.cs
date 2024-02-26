414e6e0e4df5f02b25547f957b51c5e13833303e
aethagoan <aethagoan@outlook.com>
2024-02-26 15:34:17 -0700

    started working

---
+++ GasStation.Logic/Class1.cs
@@ -0,0 +1,79 @@
+﻿namespace GasStation.Logic;
+
+
+public class RNG 
+{
+    public static Random Random = new Random(42);
+}
+
+public class GasStation
+{
+    public List<Pump> Pumps {get; private set;}
+}
+
+public class Pump
+{
+    public Tank HighOctaneTank {get; private set;}
+    public Tank LowOctaneTank {get; private set;}
+
+    public bool PumpPremium(double amount){
+        HighOctaneTank.Subtract(amount);
+        return true;
+    }
+
+    public bool PumpRegular(double amount){
+        LowOctaneTank.Subtract(amount);
+        return true;
+    }
+
+    public bool PumpMixed(double amount){
+        HighOctaneTank.Subtract(amount/2);
+        LowOctaneTank.Subtract(amount/2);
+        return true;
+    }
+
+    public Pump(Tank highOctaneTank, Tank lowOctaneTank){
+        HighOctaneTank = highOctaneTank;
+        LowOctaneTank = lowOctaneTank;
+    }
+}
+
+public class Car
+{
+    public double GasDemand {get; private set;}
+    public int TimeInSeconds {get; private set}
+    
+    public Car(double gasDemand, int timeInSeconds){
+        GasDemand = gasDemand;
+        TimeInSeconds = TimeInSeconds;
+    }
+}
+
+public enum TankType {
+    HighOctane,
+    LowOctane
+}
+
+public class Tank {
+    public double TotalAmount{get; private set;}
+    public TankType Type {get; private set;}
+
+    public Tank(TankType type, double totalAmount = 10000) {
+        Type = type;
+        TotalAmount = totalAmount;
+    }
+
+    public bool Subtract(double gasAmount) {
+        
+        if (gasAmount < TotalAmount && gasAmount > 0) {
+            TotalAmount -= gasAmount;
+            return true;
+        }
+        return false;
+    }
+
+    public bool Add(double gasAmount) {
+        TotalAmount += gasAmount;
+        return true;
+    }
+}
--- README.md
+++ README.md
@@ -4,10 +4,22 @@ We are making a gas station simulation where cars arrive at a station, line up a
 Our simulation will need to keep track of stockpiles for gasoline (tanks), amount of money paid, and sort of realistically simulate the time at the pump.  
   
 ## Wanted Features  
-0. Car structure with an amount of gas requested plus the amount of time it would take to pump  
-0. Tank strucutre that can keep track/has a method to subtract from the total.  
-0. Some type of queue for cars to wait in, whether that be a literal line or available parking spaces.  
+[ ] 1. Car structure with an amount of gas requested plus the amount of time it would take to pump  
+[ ] 0. Tank strucutre that can keep track/has a method to subtract from the total.  
+[ ] 0. Some type of queue for cars to wait in, whether that be a literal line or available parking spaces.  
+
 
 
 ### Records of group sessions  
 Monday 26 February 2024 - 2:20PM -   
+
+
+# Scenario outlines
+
+1. New cars arrive at the station at a pseudo-random rate
+2. A car will remain at a pump for a pseudo-random amount of time
+3. A car will pump a pseudo-random quantity of a pseudo-randomly selected grade of gas
+4. If a pump is out of gas then a car must go to another pump
+5. If there are no available pumps a car will wait in one of the available spaces next to a pump for a pseudo-random amount of time
+6. If there are no available spaces to wait the car will drive away and give business to someone else
+
